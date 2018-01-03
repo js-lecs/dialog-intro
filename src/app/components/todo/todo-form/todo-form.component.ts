afc5d8bd06e5401da367a76fa32ca897df62fc39
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-01-03 18:05:27 +0530

    todo timer import fix

--- src/app/components/todo/todo-form/todo-form.component.ts
+++ src/app/components/todo/todo-form/todo-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormControl } from '@angular/forms/src/model';
-import { setTimeout } from 'timers';
+
 
 
 
